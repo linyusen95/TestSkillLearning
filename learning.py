d0509bebe590f953ec43b7db25e0c618e92a4787
Sam95 <401169584@qq.com>
2022-02-13 23:38:05 +0800

    数据结构：递归的折半查找

--- learning.py
+++ learning.py
@@ -1,7 +1,15 @@
-def add(a,b):
-    c = a + b
-    return c
+def search(a,key,low,high):
+    if low>high:
+        return False
+    mid = (low + high)//2
+    if key == a[mid]:
+        return mid
+    elif key < a[mid]:
+        return search(a,key,low,mid-1)
+    else:
+        return search(a,key,mid+1,high)
 
 if __name__ == '__main__':
-    c1 = add(1,2)
-    print(c1)
+    a = [1,14,23,47,55,67,81,88,90,97]
+    ret = search(a,92,1,10)
+    print("索引为%s,key为%s"%(ret,a[ret]))
